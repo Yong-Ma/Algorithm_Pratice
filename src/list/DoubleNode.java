7f03e6241b4abbdda3fa5fe1ec4ce9eab2e0f8ae
勇 <15913967074@163.com>
2021-08-18 22:45:40 +0800

    在单链表和双链表中删除倒数第 K 个节点

---
+++ src/list/DoubleNode.java
@@ -0,0 +1,18 @@
+package list;
+
+/**
+ * 双向链表
+ *
+ * @author yong
+ * @date 2021/8/18 22:40
+ */
+public class DoubleNode {
+
+    public int value;
+    public DoubleNode last;
+    public DoubleNode next;
+    public DoubleNode(int data) {
+        this.value = data;
+    }
+
+}
