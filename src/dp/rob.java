926763190eaaa0e7d7ef0696704ba134e77cad7c
mzy <15913967074@163.com>
2021-10-19 00:20:57 +0800

    修改

--- src/dp/rob.java
+++ src/dp/rob.java
@@ -7,7 +7,6 @@ package dp;
  *
  * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
  *
- *  
  *
  * 示例 1：
  *
@@ -32,9 +31,7 @@ package dp;
 public class rob {
 
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+
         int len = nums.length;
         if (len == 1) {
             return nums[0];
