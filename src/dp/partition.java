aaa5e8b35ae3b140c18567c230b6b55e3629101e
mzy <15913967074@163.com>
2021-10-25 23:12:00 +0800

    leetcode 131. 分割回文串

---
+++ src/dp/partition.java
@@ -0,0 +1,67 @@
+package dp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @ClassName partition
+ * @description:
+ * 给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。
+ * 回文串 是正着读和反着读都一样的字符串。
+ *
+ * 示例 1：
+ * 输入：s = "aab"
+ * 输出：[["a","a","b"],["aa","b"]]
+ *
+ * 示例 2：
+ * 输入：s = "a"
+ * 输出：[["a"]]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-partitioning
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: mzy
+ * @create: 2021-10-25 22:38
+ * @Version 1.0
+ **/
+public class partition {
+
+    // 用于标记从位置i到位置j是否是回文串，使用了动态规划
+    boolean[][] f;
+    List<List<String>> ret = new ArrayList<>();
+    List<String> ans = new ArrayList<>();
+    int n;
+
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        f = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(f[i], true);
+        }
+
+        // 对各子串判断是否是回文串
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                f[i][j] = (s.charAt(i) == s.charAt(j)) && f[i + 1][j - 1];
+            }
+        }
+
+        dfs(s, 0);
+        return ret;
+    }
+
+    public void dfs(String s, int i) {
+        if (i == n) {
+            ret.add(new ArrayList<>(ans));
+            return;
+        }
+        for (int j = i; j < n; ++j) {
+            if (f[i][j]) {
+                ans.add(s.substring(i, j + 1));
+                dfs(s, j + 1);
+                ans.remove(ans.size() - 1);
+            }
+        }
+    }
+}
