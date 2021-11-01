6bcce2cbc146decec67758f8f39f13d1e754c666
mzy <15913967074@163.com>
2021-11-01 23:10:04 +0800

    leetcode 575. 分糖果

---
+++ src/array/distributeCandies.java
@@ -0,0 +1,34 @@
+package array;
+
+import java.util.HashMap;
+
+/**
+ * @ClassName distributeCandies
+ * @description:
+ * Alice 有 n 枚糖，其中第 i 枚糖的类型为 candyType[i] 。Alice 注意到她的体重正在增长，所以前去拜访了一位医生。
+ * 医生建议 Alice 要少摄入糖分，只吃掉她所有糖的 n / 2 即可（n 是一个偶数）。Alice 非常喜欢这些糖，她想要在遵循医生建议的情况下，尽可能吃到最多不同种类的糖。
+ * 给你一个长度为 n 的整数数组 candyType ，返回： Alice 在仅吃掉 n / 2 枚糖的情况下，可以吃到糖的最多种类数。
+ *
+ * 示例 1：
+ * 输入：candyType = [1,1,2,2,3,3]
+ * 输出：3
+ * 解释：Alice 只能吃 6 / 2 = 3 枚糖，由于只有 3 种糖，她可以每种吃一枚。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/distribute-candies
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: mzy
+ * @create: 2021-11-01 23:02
+ * @Version 1.0
+ **/
+public class distributeCandies {
+
+    public int distributeCandies(int[] candyType) {
+        int len = candyType.length ;
+        HashMap<Integer, Integer> typeMap = new HashMap<>(16);
+        for (int a : candyType) {
+            typeMap.merge(a, 1, Integer::sum);
+        }
+        return Math.min(typeMap.size(), len / 2);
+    }
+}
--- src/array/singleNumber.java
+++ src/array/singleNumber.java
@@ -1,4 +1,4 @@
-package num;
+package array;
 
 import java.util.HashMap;
 import java.util.Map;
