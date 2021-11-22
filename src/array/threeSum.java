324bef5ea78b96e4434179c04bc42711d8d2ae4e
mzy <15913967074@163.com>
2021-11-23 00:23:00 +0800

    leetcode 15. 三数之和

--- src/array/threeSum.java
+++ src/array/threeSum.java
@@ -31,32 +31,45 @@ import java.util.List;
  * @Version 1.0
  **/
 public class threeSum {
+
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (nums == null || nums.length <= 2) return ans;
-
-        Arrays.sort(nums); // O(nlogn)
-
-        for (int i = 0; i < nums.length - 2; i++) { // O(n^2)
-            if (nums[i] > 0) break; // 第一个数大于 0，后面的数都比它大，肯定不成立了
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // 去掉重复情况
-            int target = -nums[i];
-            int left = i + 1, right = nums.length - 1;
-            while (left < right) {
-                if (nums[left] + nums[right] == target) {
-                    ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
-
-                    // 现在要增加 left，减小 right，但是不能重复，比如: [-2, -1, -1, -1, 3, 3, 3], i = 0, left = 1, right = 6, [-2, -1, 3] 的答案加入后，需要排除重复的 -1 和 3
-                    left++; right--; // 首先无论如何先要进行加减操作
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (nums[left] + nums[right] < target) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if(len <= 2) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int first = 0; first < len - 2; first++) {
+            // 第一个数大于 0，后面的数都比它大，肯定不成立了
+            if (nums[first] > 0) {
+                break;
+            }
+            // 去掉重复情况
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            int target = -nums[first];
+            int left = first + 1, rigth = len - 1;
+            while (left < rigth) {
+                if (nums[left] + nums[rigth] == target) {
+                    res.add(Arrays.asList( nums[first], nums[left], nums[rigth] ) );
+                    // 现在要增加 left，减小 right，但是不能重复，比如: [-2, -1, -1, -1, 3, 3, 3], i = 0, left = 1, right = 6, [-2, -1, 3] 的答案加入后，
+                    // 需要排除重复的 -1 和 3
                     left++;
-                } else {  // nums[left] + nums[right] > target
-                    right--;
+                    rigth--;
+                    while (left < rigth && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < rigth && nums[rigth] == nums[rigth + 1]) {
+                        rigth--;
+                    }
+                } else if(nums[left] + nums[rigth] < target) {
+                    left++;
+                } else {
+                    rigth--;
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
