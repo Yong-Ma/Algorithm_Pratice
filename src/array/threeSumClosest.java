c3a5bc0955004f546ed6669f3e6b58748ec0d7bb
mzy <15913967074@163.com>
2021-11-25 14:33:12 +0800

    leetcode 36. 有效的数独

--- src/array/threeSumClosest.java
+++ src/array/threeSumClosest.java
@@ -18,58 +18,51 @@ import java.util.Arrays;
  * 输入：nums = [0,0,0], target = 1
  * 输出：0
  *
+ * 提示：
+ * 3 <= nums.length <= 1000
+ * -1000 <= nums[i] <= 1000
+ * -104 <= target <= 104
+ *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/3sum-closest
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @author: mzy
  * @create: 2021-11-09 23:16
- * @Version 1.0
+ * @Version 1.1
  **/
 public class threeSumClosest {
 
     public int threeSumClosest(int[] nums, int target) {
+        int closeSum = 3001;
+        int len = nums.length;
         Arrays.sort(nums);
-        int n = nums.length;
-        int best = 10000000;
-
-        // 枚举 a
-        for (int i = 0; i < n; ++i) {
-            // 保证和上一次枚举的元素不相等
+        for (int i = 0; i < len - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            // 使用双指针枚举 b 和 c
-            int j = i + 1, k = n - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                // 如果和为 target 直接返回答案
+            int left = i + 1, right = len - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == target) {
-                    return target;
+                    return sum;
                 }
-                // 根据差值的绝对值来更新答案
-                if (Math.abs(sum - target) < Math.abs(best - target)) {
-                    best = sum;
+                if(Math.abs(target - closeSum) > Math.abs(target - sum) ) {
+                    closeSum = sum;
                 }
                 if (sum > target) {
-                    // 如果和大于 target，移动 c 对应的指针
-                    int k0 = k - 1;
-                    // 移动到下一个不相等的元素
-                    while (j < k0 && nums[k0] == nums[k]) {
-                        --k0;
+                    right--;
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
                     }
-                    k = k0;
                 } else {
-                    // 如果和小于 target，移动 b 对应的指针
-                    int j0 = j + 1;
-                    // 移动到下一个不相等的元素
-                    while (j0 < k && nums[j0] == nums[j]) {
-                        ++j0;
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
                     }
-                    j = j0;
                 }
             }
         }
-        return best;
+        return closeSum;
     }
 
 }
