d68f7710d7a46cf4f57da2b51ae57677ea6fbdd0
mzy <15913967074@163.com>
2021-12-11 23:34:38 +0800

    leetcode 84. 柱状图中最大的矩形

---
+++ src/stack_queue/largestRectangleArea.java
@@ -0,0 +1,62 @@
+package stack_queue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @ClassName largestRectangleArea
+ * @description:
+ * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ *
+ * 示例 1:
+ * 输入：heights = [2,1,5,6,2,3]
+ * 输出：10
+ *
+ * 示例 2：
+ * 输入： heights = [2,4]
+ * 输出： 4
+ *
+ * 提示：
+ * 1 <= heights.length <=105
+ * 0 <= heights[i] <= 104
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: mzy
+ * @create: 2021-12-11 22:55
+ * @Version 1.0
+ **/
+public class largestRectangleArea {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        if (n == 1) {
+            return heights[0];
+        }
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ?  -1 : stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ?  n : stack.peek();
+            stack.push(i);
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return res;
+    }
+}
