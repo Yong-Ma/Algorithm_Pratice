e1d05b91d71c8dee2f975f2cc9a6f2335fc61432
mzy <15913967074@163.com>
2021-12-26 17:56:50 +0800

    leetcode 583. 两个字符串的删除操作

---
+++ src/str/minDistance.java
@@ -0,0 +1,45 @@
+package str;
+
+/**
+ * @ClassName minDistance
+ * @description:
+ * 给定两个单词word1和word2，找到使得word1和word2相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+ *
+ * 示例：
+ * 输入: "sea", "eat"
+ * 输出: 2
+ * 解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+ *
+ * 提示：
+ * 给定单词的长度不超过500。
+ * 给定单词中的字符只含有小写字母。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/delete-operation-for-two-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: mzy
+ * @create: 2021-12-26 16:42
+ * @Version 1.0
+ **/
+public class minDistance {
+
+    // 最长公共字串
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            char c1 = word1.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        // 最长公共字串的长度
+        int lcs = dp[m][n];
+        return m - lcs + n - lcs;
+    }
+}
