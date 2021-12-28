b5b19d17594eba8ab939881e3c4135d16790026e
mzy <15913967074@163.com>
2021-12-28 22:21:36 +0800

    leetcode 556. 下一个更大元素 III

---
+++ src/num/nextGreaterElement.java
@@ -0,0 +1,103 @@
+package num;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @ClassName nextGreaterElement
+ * @description:
+ * 给你一个正整数n ，请你找出符合条件的最小整数，其由重新排列 n中存在的每位数字组成，并且其值大于 n 。如果不存在这样的正整数，则返回 -1 。
+ * 注意 ，返回的整数应当是一个 32 位整数 ，如果存在满足题意的答案，但不是 32 位整数 ，同样返回 -1 。
+ *
+ * 示例 1：
+ * 输入：n = 12
+ * 输出：21
+ *
+ * 示例 2：
+ * 输入：n = 21
+ * 输出：-1
+ *
+ * 提示：
+ * 1 <= n <= 231 - 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/next-greater-element-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: mzy
+ * @create: 2021-12-28 20:40
+ * @Version 1.0
+ **/
+public class nextGreaterElement {
+
+    public String swap(String s, int i0, int i1) {
+        if (i0 == i1)
+            return s;
+        String s1 = s.substring(0, i0);
+        String s2 = s.substring(i0 + 1, i1);
+        String s3 = s.substring(i1 + 1);
+        return s1 + s.charAt(i1) + s2 + s.charAt(i0) + s3;
+    }
+
+    private ArrayList<String> list = new ArrayList<>();
+
+    private void permute(String a, int l, int r) {
+        int i;
+        if (l == r)
+            list.add(a);
+        else {
+            for (i = l; i <= r; i++) {
+                a = swap(a, l, i);
+                permute(a, l + 1, r);
+                a = swap(a, l, i);
+            }
+        }
+    }
+
+    // 暴力法，时间、空间复杂度O(n!)，会超出时间限制
+    public int nextGreaterElement1(int n) {
+        String s = "" + n;
+        permute(s, 0, s.length() - 1);
+        Collections.sort(list);
+        int i;
+        for (i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i).equals("" + n))
+                break;
+        }
+        return i == list.size() - 1 ? -1 : Integer.parseInt(list.get(i + 1));
+    }
+
+    // 线性时间、空间复杂度
+    public int nextGreaterElement2(int n) {
+        char[] a = ("" + n).toCharArray();
+        int i = a.length - 2;
+        while (i >= 0 && a[i + 1] <= a[i]) {
+            i--;
+        }
+        if (i < 0)
+            return -1;
+        int j = a.length - 1;
+        while (j >= 0 && a[j] <= a[i]) {
+            j--;
+        }
+        swap(a, i, j);
+        reverse(a, i + 1);
+        try {
+            return Integer.parseInt(new String(a));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+    private void reverse(char[] a, int start) {
+        int i = start, j = a.length - 1;
+        while (i < j) {
+            swap(a, i, j);
+            i++;
+            j--;
+        }
+    }
+    private void swap(char[] a, int i, int j) {
+        char temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
