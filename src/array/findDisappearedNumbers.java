73d663060c5e935f849819309d06bfd6d8ab3569
mzy <15913967074@163.com>
2022-01-06 23:51:51 +0800

    leetcode 448. 找到所有数组中消失的数字

---
+++ src/array/findDisappearedNumbers.java
@@ -0,0 +1,43 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName findDisappearedNumbers
+ * @description:
+ * 给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
+ *
+ * 示例 1：
+ * 输入：nums = [4,3,2,7,8,2,3,1]
+ * 输出：[5,6]
+ *
+ * 示例 2：
+ * 输入：nums = [1,1]
+ * 输出：[2]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: mzy
+ * @create: 2022-01-06 23:44
+ * @Version 1.0
+ **/
+public class findDisappearedNumbers {
+
+    // 原地修改
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        ArrayList<Integer> ret = new ArrayList<>();
+        int len = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % len;
+            nums[x] += len;
+        }
+        for (int i = 0; i < len; i++) {
+            if (nums[i] <= len) {
+                ret.add(i + 1);
+            }
+        }
+        return ret;
+    }
+}
