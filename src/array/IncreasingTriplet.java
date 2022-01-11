3365966baa6a329bff36facbc148b0ac921714c8
mzy <15913967074@163.com>
2022-01-12 01:08:31 +0800

    leetcode 334. 递增的三元子序列

---
+++ src/array/IncreasingTriplet.java
@@ -0,0 +1,55 @@
+package array;
+
+/**
+ * @ClassName IncreasingTriplet
+ * @description:
+ * 给你一个整数数组nums ，判断这个数组中是否存在长度为 3 的递增子序列。
+ * 如果存在这样的三元组下标 (i, j, k)且满足 i < j < k ，使得nums[i] < nums[j] < nums[k] ，返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3,4,5]
+ * 输出：true
+ * 解释：任何 i < j < k 的三元组都满足题意
+ *
+ * 示例 2：
+ * 输入：nums = [5,4,3,2,1]
+ * 输出：false
+ * 解释：不存在满足题意的三元组
+ *
+ * 示例 3：
+ * 输入：nums = [2,1,5,0,4,6]
+ * 输出：true
+ * 解释：三元组 (3, 4, 5) 满足题意，因为 nums[3] == 0 < nums[4] == 4 < nums[5] == 6
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/increasing-triplet-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: mzy
+ * @create: 2022-01-12 00:38
+ * @Version 1.0
+ **/
+public class IncreasingTriplet {
+
+    public boolean increasingTriplet(int[] nums) {
+        int len = nums.length;
+        if (len < 3) {
+            return false;
+        }
+        int[] leftMin = new int[len], rightMax = new int[len];
+        leftMin[0] = nums[0];
+        for (int i = 1; i < len; i++) {
+            leftMin[i] = Math.min(nums[i], leftMin[i - 1]);
+        }
+        rightMax[len - 1] = nums[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+        }
+        for (int i = 1; i < len - 1; i++) {
+            if (nums[i] > leftMin[i - 1] && nums[i] < rightMax[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
