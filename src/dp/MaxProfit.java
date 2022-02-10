78c53c99ae64c18dc99121b35f8f1ce806ff65c8
mzy <15913967074@163.com>
2022-02-10 23:25:26 +0800

    Leetcode 309. 最佳买卖股票时机含冷冻期

---
+++ src/dp/MaxProfit.java
@@ -0,0 +1,74 @@
+package dp;
+
+/**
+ * @ClassName MaxProfit
+ * @description:
+ * 给定一个整数数组prices，其中第prices[i]表示第i天的股票价格 。
+ * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+ * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ *
+ * 示例 1:
+ * 输入: prices = [1,2,3,0,2]
+ * 输出: 3
+ * 解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+ *
+ * 示例 2:
+ * 输入: prices = [1]
+ * 输出: 0
+ *
+ * 提示：
+ * 1 <= prices.length <= 5000
+ * 0 <= prices[i] <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: mzy
+ * @create: 2022-02-10 23:07
+ * @Version 1.0
+ **/
+public class MaxProfit {
+
+    // 时间空间复杂度为O(N)
+    public int maxProfit1(int[] prices) {
+
+        int len = prices.length;
+        if (len == 0) {
+            return 0;
+        }
+        int[][] f = new int[len][3];
+        // f[i][0]: 手上持有股票的最大收益
+        // f[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
+        // f[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
+        f[0][0] = - prices[0];
+        for (int i = 1; i < len; i++) {
+            f[i][0] = Math.max(f[i - 1][0], f[i - 1][2] - prices[i]);
+            f[i][1] = f[i - 1][0] + prices[i];
+            f[i][2] = Math.max(f[i - 1][2], f[i - 1][1]);
+        }
+        return Math.max(f[len - 1][1], f[len - 1][2]);
+    }
+
+    // 优化空间复杂度O（1）
+    public int maxProfit2(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        int f0 = -prices[0];
+        int f1 = 0;
+        int f2 = 0;
+        for (int i = 1; i < n; ++i) {
+            int newf0 = Math.max(f0, f2 - prices[i]);
+            int newf1 = f0 + prices[i];
+            int newf2 = Math.max(f1, f2);
+            f0 = newf0;
+            f1 = newf1;
+            f2 = newf2;
+        }
+
+        return Math.max(f1, f2);
+    }
+}
