deb6464fd5663b8cdfb3babd3f48e07032112c24
mzy <15913967074@163.com>
2022-02-20 16:19:25 +0800

    Leetcode update

--- src/dp/gerYanghuiTriangleRow.java
+++ src/dp/gerYanghuiTriangleRow.java
@@ -7,18 +7,17 @@ import java.util.List;
  * @ClassName gerYanghuiTriangleRow
  * @description:
  * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
- *
  * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
- * 示例 1:
  *
+ * 示例 1:
  * 输入: rowIndex = 3
  * 输出: [1,3,3,1]
- * 示例 2:
  *
+ * 示例 2:
  * 输入: rowIndex = 0
  * 输出: [1]
- * 示例 3:
  *
+ * 示例 3:
  * 输入: rowIndex = 1
  * 输出: [1,1]
  *
@@ -32,7 +31,7 @@ import java.util.List;
 public class gerYanghuiTriangleRow {
 
     // 递推
-    public List<Integer> getRow(int rowIndex) {
+    public List<Integer> getRow1(int rowIndex) {
 
         ArrayList<Integer> res = new ArrayList<>();
         res.add(1);
@@ -43,6 +42,21 @@ public class gerYanghuiTriangleRow {
             }
         }
         return res;
+    }
+    public List<Integer> getRow2(int rowIndex) {
+        List<Integer> pre = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; ++i) {
+            List<Integer> cur = new ArrayList<>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    cur.add(1);
+                } else {
+                    cur.add(pre.get(j - 1) + pre.get(j));
+                }
+            }
+            pre = cur;
+        }
+        return pre;
     }
 
 }
